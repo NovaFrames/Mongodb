56f14289a463fa52dd2182587a542badf36b6cc2
stalinchidhamparam <stalinchidhamparam@gmail.com>
2025-12-20 20:43:07 +0530

    Update server.js

--- mongodb/server.js
+++ mongodb/server.js
@@ -3,11 +3,14 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/users", require("./routes/userRoutes"));
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
